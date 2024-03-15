bcfa6dd5469e3589c21ff9dab066200bbb66006c
ZioPao <picchetti1998@gmail.com>
2024-03-15 19:51:54 +0100

    json shop prices gen

---
+++ generate_full_json.py
@@ -0,0 +1,112 @@
+import json
+import re
+
+
+with open('output/prices.json') as json_file:
+    prices_dict = json.load(json_file)
+
+with open('output/tags.json') as json_file:
+    tags_dict = json.load(json_file)
+
+
+# Sort dicts
+tags_dict = sorted(tags_dict, key=lambda d: d['fullType'])
+prices_dict = sorted(prices_dict, key=lambda d: d['fullType'])
+
+r = re.compile(r'sfx', re.I)
+
+json_list = []
+for i in range(0, len(prices_dict)):
+    row_price = prices_dict[i]
+    row_tags = tags_dict[i]
+
+    fType = row_price['fullType']
+    price = row_price['price']
+    tag = row_tags['tag']
+
+    temp_json = {}
+
+    if fType != "Base.GranolaBar" and fType != "Base.WaterBottleFull" and fType != "Base.Cereal"\
+            and fType != "Base.Butter" and fType != "Base.Baseballbat" and fType != "Base.Crowbar"\
+            and fType != "Base.ShotgunSawnoff" and fType != "Base.ShotgunShellsBox"\
+            and fType != "Base.ShotgunShellsBox" and fType != "Base.Pistol" and fType != "Base.9mmClip"\
+            and fType != "Base.Bullets9mmBox" and fType != "Base.Bandage":
+
+        if not r.findall(fType):
+
+            temp_json['fullType'] = fType
+            temp_json['tag'] = tag
+            temp_json['basePrice'] = price
+
+            json_list.append(temp_json)
+
+
+# Add essential items with fixed prices
+
+
+essentials = [
+    {'fullType': "Base.GranolaBar",
+     'tag': 'ESSENTIAL',
+     'basePrice': 20},
+
+    {'fullType': "Base.WaterBottleFull",
+     'tag': 'ESSENTIAL',
+     'basePrice': 50},
+
+    {'fullType': "Base.Cereal",
+     'tag': 'ESSENTIAL',
+     'basePrice': 20},
+
+    {'fullType': "Base.Butter",
+     'tag': 'ESSENTIAL',
+     'basePrice': 500},
+
+    {'fullType': "Base.BaseballBat",
+     'tag': 'ESSENTIAL',
+     'basePrice': 200},
+
+    {'fullType': "Base.Crowbar",
+     'tag': 'ESSENTIAL',
+     'basePrice': 1000},
+
+    {'fullType': "Base.ShotgunSawnoff",
+     'tag': 'ESSENTIAL',
+     'basePrice': 1500},
+
+    {'fullType': "Base.ShotgunShellsBox",
+     'tag': 'ESSENTIAL',
+     'basePrice': 250},
+
+    {'fullType': "Base.Pistol",
+     'tag': 'ESSENTIAL',
+     'basePrice': 750},
+
+    {'fullType': "Base.9mmClip",
+     'tag': 'ESSENTIAL',
+     'basePrice': 250},
+
+    {'fullType': "Base.Bullets9mmBox",
+     'tag': 'ESSENTIAL',
+     'basePrice': 250},
+
+    {'fullType': "Base.Bandage",
+     'tag': 'ESSENTIAL',
+     'basePrice': 100},
+
+]
+
+for i in range(0, len(essentials)):
+
+    row = essentials[i]
+    temp_json = {}
+    temp_json['fullType'] = row['fullType']
+    temp_json['tag'] = row['tag']
+    temp_json['basePrice'] = row['basePrice']
+
+    json_list.append(temp_json)
+
+
+
+with open('output/shop_items.json', 'w') as file:
+    # file.write(code)
+    json.dump(json_list, file, indent=4)
---
+++ import_lua_data.py
@@ -0,0 +1,2 @@
+# todo reimport data from lua code
+
